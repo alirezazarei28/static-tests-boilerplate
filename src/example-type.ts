f346fefd9e48bfd90cf98dda18513ee328bceaef
Alireza <alirezazarei28@gmail.com>
2022-02-19 12:17:29 +0330

    lint, prettier, husky added

---
+++ src/example-type.ts
@@ -0,0 +1,12 @@
+const username = 'Alireza';
+
+typeof username === 'string';
+
+console.log(username);
+
+function printUser(a: string) {
+  return a + '1';
+}
+
+printUser('1');
+console.log('✅❌' + username);
